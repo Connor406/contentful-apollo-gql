c5f3b71705be50f0d908dc4db44ec8ea64159893
Connor Mahan <connor@reachcreative.com>
2022-06-13 13:40:22 -0500

    🤠

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,15 @@ import type { AppProps } from 'next/app';
 import { ApolloProvider } from '@apollo/client';
 import '@/styles/globals.scss';
 import Navbar from '@/components/Navbar';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ApolloProvider client={client}>
+      <Head>
+        <title>Contenful Example</title>
+        <link rel="icon" href="/favicon.png" />
+      </Head>
       <Component {...pageProps} />
       <Navbar />
     </ApolloProvider>
